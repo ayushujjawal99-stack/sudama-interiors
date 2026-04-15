250da9e5048398fcaf38dbc8689d8b36c1f9a93b
ayushujjawal99-stack <ayushujjawal99@gmail.com>
2026-04-16 03:41:45 +0530

    remove admin reset endpoint

--- sudama/urls.py
+++ sudama/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from core.views import create_admin
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('services/', include('services.urls', namespace='services')),
     path('products/', include('products.urls', namespace='products')),
     path('', include('core.urls')),
-    path('create-admin/', create_admin),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
