c03579e147dbcb36d98fc4ab77f1f6baef8d7891
ayushujjawal99-stack <ayushujjawal99@gmail.com>
2026-04-16 04:00:49 +0530

    run migrations on deploy

--- sudama/settings.py
+++ sudama/settings.py
@@ -60,13 +60,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'sudama.wsgi.application'
 
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
