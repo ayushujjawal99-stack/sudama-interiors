a6a33398673ba28a8488c41a44a00e37a6abec7f
ayushujjawal99-stack <ayushujjawal99@gmail.com>
2026-04-16 10:50:18 +0530

    check storage config

--- sudama/settings.py
+++ sudama/settings.py
@@ -114,3 +114,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 WHITENOISE_MANIFEST_STRICT = False
+
+print("STORAGE:", STORAGES)
+print("DEBUG:", DEBUG)
