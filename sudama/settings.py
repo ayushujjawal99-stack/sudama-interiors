3363ceefae3f630742016fe91a5197dc2fdea260
ayushujjawal99-stack <ayushujjawal99@gmail.com>
2026-04-24 11:46:54 +0530

    debug enable

--- sudama/settings.py
+++ sudama/settings.py
@@ -145,3 +145,19 @@ LOGGING = {
         },
     },
 }
+
+import logging
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "ERROR",
+    },
+}
