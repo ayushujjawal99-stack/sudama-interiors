c24d2929fd7a4f963ae95267ede6a218a2160bbe
ayushujjawal99-stack <ayushujjawal99@gmail.com>
2026-05-17 20:40:17 +0530

    changes

--- sudama/settings.py
+++ sudama/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'sudama.wsgi.application'
 
 # DATABASE
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # PASSWORD VALIDATION
